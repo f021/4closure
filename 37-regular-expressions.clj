3c4446e01a36f3c71b4b2f2dc35a7d029679007b
andrei <andrei.fzto@gmail.com>
2015-12-20 17:56:35 +0300

    cont. elementary

---
+++ 37-regular-expressions.clj
@@ -0,0 +1,3 @@
+;; Regex patterns are supported with a special reader macro.
+
+(= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
---
+++ 52-intro-to-destructuring.clj
@@ -0,0 +1,4 @@
+;; Let bindings and function parameter lists support destructuring.
+
+
+(= [2 4] (let [[a b c d e] [0 1 2 3 4]] [c e]))
